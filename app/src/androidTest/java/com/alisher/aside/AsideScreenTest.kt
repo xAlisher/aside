99ac5aa18fa8d8dcc3d2fe5bc4467728293ff309
Alisher <alisher@status.im>
2025-05-20 07:11:08 +0200

    Add Compose UI test for main screen

---
+++ app/src/androidTest/java/com/alisher/aside/AsideScreenTest.kt
@@ -0,0 +1,24 @@
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.assertIsDisplayed
+import org.junit.Rule
+import org.junit.Test
+import com.alisher.aside.ui.theme.AsideTheme
+import com.alisher.aside.AsideScreen
+
+class AsideScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun mainButtonsAreDisplayed() {
+        composeTestRule.setContent {
+            AsideTheme {
+                AsideScreen()
+            }
+        }
+
+        composeTestRule.onNodeWithText("⎘ Paste").assertIsDisplayed()
+        composeTestRule.onNodeWithText("+ Create").assertIsDisplayed()
+    }
+}
