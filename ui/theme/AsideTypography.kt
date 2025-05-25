3aa17cc1e771e00ac728b5b8a0cc32bd6996f7d1
Alisher <alisher@status.im>
2025-05-25 04:28:55 +0200

    Add typography tokens and integrate into Aside theme

---
+++ ui/theme/AsideTypography.kt
@@ -0,0 +1,10 @@
+package com.alisher.aside.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AsideTypography = Typography(
+    headlineMedium = Headline,
+    bodyMedium = Body,
+    bodyLarge = Messages,
+    labelSmall = Tag
+)
