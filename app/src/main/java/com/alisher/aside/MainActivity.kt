6cf36d6c0d584bd631c98792e8ba6192d95dbb04
Alisher <alisher@status.im>
2025-05-31 08:26:46 +0200

    Add SessionTestScreen and show in MainActivity

--- app/src/main/java/com/alisher/aside/MainActivity.kt
+++ app/src/main/java/com/alisher/aside/MainActivity.kt
@@ -3,15 +3,15 @@ package com.alisher.aside
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.alisher.aside.ui.debug.SessionTestScreen
 import com.alisher.aside.ui.theme.*
-import com.alisher.aside.AsideScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AsideTheme {
-                AsideScreen()
+                SessionTestScreen()
             }
         }
     }
